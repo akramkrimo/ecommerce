27ac8f9546c1639bdd7d07f0524d220cc38fd0fd
Akram <>
2021-01-05 07:00:26 -0500

    finxing the static serving during development

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -10,4 +9,8 @@ urlpatterns = [
     path('payment/', include('payments.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
